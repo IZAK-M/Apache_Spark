c38c2d876ad874db00cc0452ab558036848808c2
IZAK-M <izakmohamed44@gmail.com>
2025-09-28 20:08:23 +0200

    pyspark session initialisation

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import SparkSession from pyspark.sql
+from pyspark.sql import SparkSession
+
+# Creation de my_spark
+my_spark = SparkSession.builder.appName("my_spark").getOrCreate()
+
+# Print my_spark
+print(my_spark)
+
+# Lecture du fichier csv:
+ventes_df = my_spark.read.csv("ventes.csv", header=True, inferSchema=True)
+
+ventes_df.show()
